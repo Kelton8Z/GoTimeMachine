7d90b0f701a718792d4c3e1a573c65e3fc2f1025
Kelton Zhang <zhuoran2@andrew.cmu.edu>
2023-03-02 16:41:38 -0800

    comment out screen part

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 	"fmt"
 	"os/exec"
 	"runtime"
-	"strings"
 	"time"
 
 	// . "github.com/go-git/go-git/v5/_examples"
 	"github.com/go-errors/errors"
 	"github.com/progrium/macdriver/cocoa"
-	"github.com/progrium/macdriver/core"
 	"github.com/progrium/macdriver/objc"
 )
 
@@ -34,7 +32,7 @@ func main() {
 	runtime.LockOSThread()
 
 	app := cocoa.NSApp_WithDidLaunch(func(n objc.Object) {
-		fontName := flag.String("font", "Helvetica", "font to use")
+		// fontName := flag.String("font", "Helvetica", "font to use")
 		flag.Parse()
 
 		obj := cocoa.NSStatusBar_System().StatusItemWithLength(cocoa.NSVariableStatusItemLength)
@@ -52,7 +50,7 @@ func main() {
 		itemPause := cocoa.NSMenuItem_New()
 		itemPause.SetTitle("Pause")
 		itemPause.SetAction(objc.Sel("pause:"))
-		cocoa.DefaultDelegateClass.AddMethod("trace:", func(_ objc.Object) {
+		cocoa.DefaultDelegateClass.AddMethod("pause:", func(_ objc.Object) {
 			mode = PAUSE
 			fmt.Println("pause!")
 		})
@@ -71,64 +69,60 @@ func main() {
 		menu.AddItem(itemTrace)
 		obj.SetMenu(menu)
 
-		screen := cocoa.NSScreen_Main().Frame().Size
-		text := fmt.Sprintf(" %s ", strings.Join(flag.Args(), " "))
-		tr, fontSize := func() (rect core.NSRect, size float64) {
-			t := cocoa.NSTextView_Init(core.Rect(0, 0, 0, 0))
-			t.SetString(text)
-			for s := 70.0; s <= 550; s += 12 {
-				t.SetFont(cocoa.Font(*fontName, s))
-				t.LayoutManager().EnsureLayoutForTextContainer(t.TextContainer())
-				rect = t.LayoutManager().UsedRectForTextContainer(t.TextContainer())
-				size = s
-				if rect.Size.Width >= screen.Width {
-					break
+		/*
+			screen := cocoa.NSScreen_Main().Frame().Size
+			text := fmt.Sprintf(" %s ", strings.Join(flag.Args(), " "))
+			tr, fontSize := func() (rect core.NSRect, size float64) {
+				t := cocoa.NSTextView_Init(core.Rect(0, 0, 0, 0))
+				t.SetString(text)
+				for s := 70.0; s <= 550; s += 12 {
+					t.SetFont(cocoa.Font(*fontName, s))
+					t.LayoutManager().EnsureLayoutForTextContainer(t.TextContainer())
+					rect = t.LayoutManager().UsedRectForTextContainer(t.TextContainer())
+					size = s
+					if rect.Size.Width >= screen.Width {
+						break
+					}
 				}
-			}
-			return rect, size
-		}()
+				return rect, size
+			}()
 
-		t := cocoa.NSTextView_Init(tr)
-		t.SetString(text)
-		t.SetFont(cocoa.Font(*fontName, fontSize))
-		t.SetEditable(false)
-		t.SetImportsGraphics(false)
-		t.SetDrawsBackground(false)
+			t := cocoa.NSTextView_Init(tr)
+			t.SetString(text)
+			t.SetFont(cocoa.Font(*fontName, fontSize))
+			t.SetEditable(false)
+			t.SetImportsGraphics(false)
+			t.SetDrawsBackground(false)
 
-		c := cocoa.NSView_Init(core.Rect(0, 0, 0, 0))
-		c.SetBackgroundColor(cocoa.Color(0, 0, 0, 0.75))
-		c.SetWantsLayer(true)
-		c.Layer().SetCornerRadius(32.0)
-		c.AddSubviewPositionedRelativeTo(t, cocoa.NSWindowAbove, nil)
+			c := cocoa.NSView_Init(core.Rect(0, 0, 0, 0))
+			c.SetBackgroundColor(cocoa.Color(0, 0, 0, 0.75))
+			c.SetWantsLayer(true)
+			c.Layer().SetCornerRadius(32.0)
+			c.AddSubviewPositionedRelativeTo(t, cocoa.NSWindowAbove, nil)
 
-		tr.Size.Height = screen.Height
-		tr.Size.Width = screen.Width
-		tr.Origin.X = (screen.Width / 2) - (tr.Size.Width / 2)
-		tr.Origin.Y = (screen.Height / 2) - (tr.Size.Height / 2)
+			tr.Size.Height = screen.Height
+			tr.Size.Width = screen.Width
+			tr.Origin.X = (screen.Width / 2) - (tr.Size.Width / 2)
+			tr.Origin.Y = (screen.Height / 2) - (tr.Size.Height / 2)
 
-		w := cocoa.NSWindow_Init(core.Rect(0, 0, 0, 0),
-			cocoa.NSWindowStyleMaskFullScreen, cocoa.NSBackingStoreBuffered, false)
-		w.SetContentView(c)
-		w.SetTitlebarAppearsTransparent(true)
-		w.SetTitleVisibility(cocoa.NSWindowTitleHidden)
-		w.SetOpaque(false)
-		w.SetBackgroundColor(cocoa.NSColor_Clear())
-		w.SetLevel(cocoa.NSMainMenuWindowLevel + 2)
-		w.SetFrameDisplay(tr, true)
-		cls := objc.NewClass("AppDelegate", "NSWindow")
-		cls.AddMethod("applicationDidFinishLaunching:", func(app objc.Object) {
-			w.ToggleFullScreen(nil)
-			fmt.Println("Launched!")
-		})
+			w := cocoa.NSWindow_Init(core.Rect(0, 0, 0, 0),
+				cocoa.NSWindowStyleMaskFullScreen, cocoa.NSBackingStoreBuffered, false)
+			w.SetContentView(c)
+			w.SetTitlebarAppearsTransparent(true)
+			w.SetTitleVisibility(cocoa.NSWindowTitleHidden)
+			w.SetOpaque(false)
+			w.SetBackgroundColor(cocoa.NSColor_Clear())
+			w.SetLevel(cocoa.NSMainMenuWindowLevel + 2)
+			w.SetFrameDisplay(tr, true)
+			cls := objc.NewClass("AppDelegate", "NSWindow")
+			cls.AddMethod("applicationDidFinishLaunching:", func(app objc.Object) {
+				w.ToggleFullScreen(nil)
+				fmt.Println("Launched!")
+			})
 
-		w.MakeKeyAndOrderFront(nil)
-		objc.RegisterClass(cls)
-
-		delegate := objc.Get("AppDelegate").Alloc().Init()
-		app := objc.Get("NSApplication").Get("sharedApplication")
-		app.Set("delegate:", delegate)
-		app.Send("run")
-
+			w.MakeKeyAndOrderFront(nil)
+			objc.RegisterClass(cls)
+		*/
 		// events := make(chan cocoa.NSEvent, 64)
 		// go func() {
 		// 	<-events
@@ -148,7 +142,7 @@ func main() {
 
 	cd_cmd.Run()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 80*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	output_file := "rewindMe" + time.Now().String() + ".mkv"
